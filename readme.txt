c43b2725048793dd3d91d2c515d18a351bcaa682
tamlyn <tamlyn@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-08-25 20:01:52 +0000

    Preparing for 1.1 release
    git-svn-id: https://plugins.svn.wordpress.org/unattach/trunk@972638 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- readme.txt
+++ readme.txt
@@ -2,8 +2,8 @@
 Contributors: tamlyn
 Tags: unattach, detach, media, attachments
 Requires at least: 3.1
-Tested up to: 3.5
-Stable tag: 1.0.1
+Tested up to: 3.9
+Stable tag: 1.1
 
 Allows detaching images and other media from posts, pages and other content types.
 
@@ -11,7 +11,7 @@ Allows detaching images and other media from posts, pages and other content type
 
 Unattach is a very simple plugin which allows detaching images and other media from WordPress posts, pages and other content types.
 
-As of WordPress 3.5, the only way to detach a media item from a post is to delete either the post or the media item. This plugin solves that by adding an 'Unattach' link below each media file in the Media Library admin view.
+The only way to detach a media item from a post in WordPress is to delete either the post or the media item. This plugin solves that by adding an 'Unattach' link below each media file in the Media Library admin view.
 
 [Plugin by Outlandish Ideas](http://outlandishideas.co.uk/blog/2011/03/unattach/)
 
@@ -23,6 +23,15 @@ As of WordPress 3.5, the only way to detach a media item from a post is to delet
 4. Click activate.
 
 == ChangeLog ==
+
+= Version 1.1 =
+
+* Added translation support
+* Added German translation
+* Fixed CSRF bug
+* Made capability configurable with `unattach_capability` filter.
+
+Thanks to Ov3rfly for these contributions.
 
 = Version 1.0.1 =
 
--- unattach.php
+++ unattach.php
@@ -3,14 +3,13 @@
 Plugin Name:  Unattach
 Plugin URI:   http://outlandishideas.co.uk/blog/2011/03/unattach/
 Description:  Allows detaching images and other media from posts, pages and other content types.
-Version:      1.0.2
-Author:       tamlyn, Ov3rfly
+Version:      1.1
+Author:       tamlyn
 **************************************************************************/
 
 //filter to add button to media library UI
 function unattach_media_row_action( $actions, $post ) {
 	if ($post->post_parent) {
-		// 1.0.2: add nonce to fix CSRF, Ov3rfly
 		$url = wp_nonce_url( admin_url('tools.php?page=unattach&noheader=true&id=' . $post->ID), 'unattach' );
 		$actions['unattach'] = '<a href="' . esc_url( $url ) . '" title="' . __( 'Unattach this media item.', 'unattach' ) . '">' . __( 'Unattach', 'unattach' ) . '</a>';
 	}
@@ -20,13 +19,11 @@ function unattach_media_row_action( $actions, $post ) {
 
 //action to set post_parent to 0 on attachment
 function unattach_do_it() {
-	// 1.0.2: check nonce to fix CSRF, Ov3rfly
 	check_admin_referer( 'unattach' );
 
 	global $wpdb;
 
 	if (!empty($_REQUEST['id'])) {
-		// 1.0.2: cast id to int, Ov3rfly
 		$wpdb->update($wpdb->posts, array('post_parent'=>0), array('id'=>(int)$_REQUEST['id'], 'post_type'=>'attachment'));
 	}
 	
@@ -37,7 +34,6 @@ function unattach_do_it() {
 //set it up
 add_action( 'admin_menu', 'unattach_init' );
 function unattach_init() {
-	// 1.0.2: add filter for capability, Ov3rfly
 	$capability = apply_filters( 'unattach_capability', 'upload_files' );
 	if ( current_user_can( $capability ) ) {
 		add_filter('media_row_actions',  'unattach_media_row_action', 10, 2);
@@ -48,15 +44,14 @@ function unattach_init() {
 }
 
 /*
-// example to restrict access via capability in functions.php, Ov3rfly
-// see also http://codex.wordpress.org/Roles_and_Capabilities
-function my_unattach_capability( $capability ) {
-	return 'administrator';
-}
-add_filter( 'unattach_capability', 'my_unattach_capability' );
-*/
+ * Example to restrict access to administrators. Put this in functions.php:
+ *
+ * add_filter( 'unattach_capability', function( $capability ) {
+ *    return 'administrator';
+ * });
+ */
 
-// 1.0.2: add translation possibility, Ov3rfly
+// load translations
 function unattach_load_textdomain() {
 	load_plugin_textdomain( 'unattach', false, dirname( plugin_basename( __FILE__ ) ) . '/lang/' ); 
 }
