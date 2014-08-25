6df6ac27955b4c557265f16fb421f3819536e8db
tamlyn <tamlyn@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-08-25 21:49:08 +0000

    Preparing for 1.2 release
    git-svn-id: https://plugins.svn.wordpress.org/unattach/trunk@972677 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- readme.txt
+++ readme.txt
@@ -3,7 +3,7 @@ Contributors: tamlyn
 Tags: unattach, detach, media, attachments
 Requires at least: 3.1
 Tested up to: 3.9
-Stable tag: 1.1
+Stable tag: 1.2
 
 Allows detaching images and other media from posts, pages and other content types.
 
@@ -13,7 +13,7 @@ Unattach is a very simple plugin which allows detaching images and other media f
 
 The only way to detach a media item from a post in WordPress is to delete either the post or the media item. This plugin solves that by adding an 'Unattach' link below each media file in the Media Library admin view.
 
-[Plugin by Outlandish Ideas](http://outlandishideas.co.uk/blog/2011/03/unattach/)
+[Plugin by Outlandish](http://outlandish.com/blog/2011/03/unattach/)
 
 == Installation ==
 
@@ -24,10 +24,17 @@ The only way to detach a media item from a post in WordPress is to delete either
 
 == ChangeLog ==
 
+= Version 1.2 =
+
+* Maintain page and search term when unattaching.
+* Check edit permissions on post and media item.
+* Added German translation
+
+Thanks to Ov3rfly, ramonqu, rpb1001 and jodzee for these contributions.
+
 = Version 1.1 =
 
 * Added translation support
-* Added German translation
 * Fixed CSRF bug
 * Made capability configurable with `unattach_capability` filter.
 
--- unattach.php
+++ unattach.php
@@ -3,14 +3,27 @@
 Plugin Name:  Unattach
 Plugin URI:   http://outlandishideas.co.uk/blog/2011/03/unattach/
 Description:  Allows detaching images and other media from posts, pages and other content types.
-Version:      1.1
+Version:      1.2
 Author:       tamlyn
 **************************************************************************/
 
 //filter to add button to media library UI
 function unattach_media_row_action( $actions, $post ) {
-	if ($post->post_parent) {
-		$url = wp_nonce_url( admin_url('tools.php?page=unattach&noheader=true&id=' . $post->ID), 'unattach' );
+	// only add action if user can edit media and parent
+	if ( current_user_can( 'edit_post', $post->ID ) && $post->post_parent && current_user_can( 'edit_post', $post->post_parent ) ) {
+		$url = wp_nonce_url( admin_url('options.php?page=unattach&noheader=true&id=' . $post->ID ), 'unattach' );
+
+		//add current page and searchterm as param
+		if ( !empty( $_REQUEST['paged'] ) ) {
+			$pagenum = (int)$_REQUEST['paged'];
+			if ( $pagenum > 1 ) {
+				$url = add_query_arg( 'paged', $pagenum, $url );
+			}
+		}
+		if ( !empty( $_REQUEST['s'] ) ) {
+			$url = add_query_arg( 's', $_REQUEST['s'], $url );
+		}
+        
 		$actions['unattach'] = '<a href="' . esc_url( $url ) . '" title="' . __( 'Unattach this media item.', 'unattach' ) . '">' . __( 'Unattach', 'unattach' ) . '</a>';
 	}
 
@@ -22,12 +35,26 @@ function unattach_do_it() {
 	check_admin_referer( 'unattach' );
 
 	global $wpdb;
-
-	if (!empty($_REQUEST['id'])) {
-		$wpdb->update($wpdb->posts, array('post_parent'=>0), array('id'=>(int)$_REQUEST['id'], 'post_type'=>'attachment'));
+	if ( !empty($_REQUEST['id'] ) ) {
+		$id = (int)$_REQUEST['id'];
+		if ( current_user_can( 'edit_post', $id ) ) {
+			$wpdb->update($wpdb->posts, array('post_parent'=>0), array('id'=>$id, 'post_type'=>'attachment'));
+		}
 	}
-	
-	wp_redirect(admin_url('upload.php'));
+
+    //construct return URL
+	$url = 'upload.php';
+	if ( !empty( $_REQUEST['paged'] ) ) {
+		$pagenum = (int)$_REQUEST['paged'];
+		if ( $pagenum > 1 ) {
+			$url = add_query_arg( 'paged', $pagenum, $url );
+		}
+	}
+	if ( !empty( $_REQUEST['s'] ) ) {
+		$url = add_query_arg( 's', $_REQUEST['s'], $url );
+	}
+
+	wp_redirect( admin_url( $url ) );
 	exit;
 }
 
@@ -37,9 +64,8 @@ function unattach_init() {
 	$capability = apply_filters( 'unattach_capability', 'upload_files' );
 	if ( current_user_can( $capability ) ) {
 		add_filter('media_row_actions',  'unattach_media_row_action', 10, 2);
-		//this is hacky but couldn't find the right hook
-		add_submenu_page('tools.php', 'Unattach Media', 'Unattach', $capability, 'unattach', 'unattach_do_it');
-		remove_submenu_page('tools.php', 'unattach');
+		// 1.0.3: use null instead tools.php
+		add_submenu_page( null, 'Unattach', 'Unattach', $capability, 'unattach', 'unattach_do_it' );
 	}
 }
 
